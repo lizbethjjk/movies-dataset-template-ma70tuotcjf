386ec4068cc89b204fe1f5b82b1d28cfd1a0d342
lizbethjjk <lizbethjosephkoithra@gmail.com>
2024-07-06 16:35:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,14 @@ import pandas as pd
 import streamlit as st
 
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+st.set_page_config(page_title="Housing & Development Board Flat Types and Prices dashboard")
+st.title("HDB dataset")
 st.write(
     """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
+    This project delves into analyzing and visualizing trends in Singapore HDB resale prices spanning from 1990 to 2023, leveraging Streamlit to create interactive data/graph visualizations. The objective is to provide insightful data exploration tools for stakeholders including buyers, investors, agents, and policymakers, facilitating informed decision-making. Streamlit will serve as the primary platform for developing dynamic user interfaces with features like filtering by year, flat type, and town. These interactive capabilities will enable users to explore trends over time and across different property categories dynamically. With Streamlit, the project aims to transform static data into engaging narrative tools, enhancing user engagement and supporting comprehensive analysis in the Singapore HDB resale market context.
+
+The dataset selected for this analysis comprises comprehensive records of Singapore HDB resale prices from 1990 to 2023, available through sources like Kaggle. It includes detailed attributes such as transaction month, town, flat type (ranging from 1-room to multi-generation), flat model (e.g., New Generation, Improved), lease commencement date, storey range, floor area, remaining lease, and the resale price. This dataset offers a longitudinal perspective, spanning over three decades, enabling a thorough examination of trends in the HDB resale market. Its richness in data allows for in-depth analysis of factors influencing resale prices across different housing types, locations, and periods. Insights derived from this dataset are crucial for understanding market dynamics, and policy impacts, and informing strategic decisions for stakeholders in Singapore's housing market.
+
     """
 )
 
@@ -18,26 +19,36 @@ st.write(
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
+    df = pd.read_csv("/Users/elizabethjosephkoithara/Desktop/Kaplan/Mon_ICT305/Assg/01/HDB Dataset/resale-flat-prices-based-on-approval-date-1990-1999_locationdata.csv")
+    df = pd.read_csv("/Users/elizabethjosephkoithara/Desktop/Kaplan/Mon_ICT305/Assg/01/HDB Dataset/resale-flat-prices-based-on-approval-date-2000-feb-2012_locationdata.csv")
+    df = pd.read_csv("/Users/elizabethjosephkoithara/Desktop/Kaplan/Mon_ICT305/Assg/01/resale-flat-prices-based-on-registration-date-from-jan-2015-to-dec-2016_locationdata.csv")
+    df = pd.read_csv("/Users/elizabethjosephkoithara/Desktop/Kaplan/Mon_ICT305/Assg/01/HDB Dataset/resale-flat-prices-based-on-registration-date-from-jan-2017-onwards_locationdata.csv")
+    df = pd.read_csv("/Users/elizabethjosephkoithara/Desktop/Kaplan/Mon_ICT305/Assg/01/HDB Dataset/resale-flat-prices-based-on-registration-date-from-mar-2012-to-dec-2014_locationdata.csv")
     return df
 
 
 df = load_data()
 
 # Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
+town = st.multiselect(
+    "Town",
+    df.town.unique(),
+    ["Ang Mo Kio", "Woodlands"],
+)
+
+flat_type = st.multiselect(
+    "Flat Type",
+    df.flat_type.unique(),
+    ["1 Room", "2 Room"],
 )
 
 # Show a slider widget with the years using `st.slider`.
 years = st.slider("Years", 1986, 2006, (2000, 2016))
 
 # Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
+df_filtered = df[(df["town"].isin(town)) & (df["flat_type"].between(flat_type[0], flat_type[1]))]
 df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
+    index="flat_type", columns="town", values="gross", aggfunc="sum", fill_value=0
 )
 df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
 
@@ -46,7 +57,7 @@ df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
 st.dataframe(
     df_reshaped,
     use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
+    column_config={"years": st.column_config.TextColumn("Year")},
 )
 
 # Display the data as an Altair chart using `st.altair_chart`.
